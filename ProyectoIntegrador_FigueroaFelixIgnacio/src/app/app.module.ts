0dadea43aa2a3897a6c14d989f08596a8492e7eb
hectoref96 <fhectoremanuel@gmail.com>
2022-08-06 22:02:32 -0300

    v1
    Co-Authored-By: felix9941 <95634955+felix9941@users.noreply.github.com>

--- ProyectoIntegrador_FigueroaFelixIgnacio/src/app/app.module.ts
+++ ProyectoIntegrador_FigueroaFelixIgnacio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './componets/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
